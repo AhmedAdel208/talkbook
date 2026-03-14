439486d0559f4cbeeb838bd397032303e43ac27c
1ccount1for0utb3channe1an4aa1-web <1ccount1for0utb3channe1an4aa1@gmail.com>
2026-03-15 00:12:31 +0200

    add new book page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Serif, Mona_Sans} from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import {Toaster} from "@/components/ui/sonner";
 
